2431cd14cf76b04327714558ddb885e914b93422
Max <maim.zadoina@gmail.com>
2021-10-03 20:11:27 +0300

    Added layout HOC & Grid configuration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
 import { Button, Rating } from '../components';
-export default function Home(): JSX.Element {
+import { withLayout } from '../layout/Layout';
+function Home(): JSX.Element {
   const [rating, setRating] = useState<number>(4);
   return (
-    <div>
+    <>
       <Rating setRating={setRating} isEditable rating={rating} />
       <Button appearance="primary">ok</Button>
-    </div>
+    </>
   );
 }
+
+export default withLayout(Home);
