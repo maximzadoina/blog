7e2911b9a33e625b7337af49ec2a7820010042a1
Max <maim.zadoina@gmail.com>
2021-10-03 15:21:00 +0300

    Added Button, Tag, Paragraph, Rating, H components

--- pages/dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-function Home() {
-    return (React.createElement("div", null));
-}
-exports["default"] = Home;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,11 @@
+import React, { useState } from 'react';
+import { Button, Rating } from '../components';
 export default function Home(): JSX.Element {
-  return <div></div>;
+  const [rating, setRating] = useState<number>(4);
+  return (
+    <div>
+      <Rating setRating={setRating} isEditable rating={rating} />
+      <Button appearance="primary">ok</Button>
+    </div>
+  );
 }
