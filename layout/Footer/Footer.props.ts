6c2edc220da4b76471fa8f60693f770c7d7d14a5
Max <maim.zadoina@gmail.com>
2021-10-24 20:59:55 +0200

    Added footer layout

--- layout/Footer/Footer.props.ts
+++ layout/Footer/Footer.props.ts
@@ -1,3 +1,5 @@
 import { DetailedHTMLProps, HTMLAttributes, ReactNode } from 'react';
 
-export interface FooterProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {}
+export interface FooterProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+  children: ReactNode;
+}
